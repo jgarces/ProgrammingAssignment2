f63181e45633a76be6cd42e591bd7bf0c142c2be
jgarces <garces_jose_luis@hotmail.com>
2014-06-18 00:53:36 +0800

    Extended comments with a TODO to look into tomorrow

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,16 @@
-## This function library provided two functions to assist in the c
-## 
+## This function library provided two functions to assist in the computation
+## of the inverse of square matrix with the ability to cache previously determined
+## to void the costly computation associated
 
-## Write a short comment describing this function
+## Usage Example:
+## aMatrix <- stats::rnorm(16)
+## dim(aMatrix) <- c(4,4)
+## aCacheMatrix <- makeCacheMatrix(aMatrix)
+## cacheSolve(aCacheMatrix)
 
+## This function create custom matrix object that can cache the inversion of the
+## matrix. Note this function expects a square numeric matrix 
+## TODO: Check the matrix is a square matrix
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y){
@@ -16,8 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function determines the inverse of a CacheMatrix or return previously determined
+## inverse that has been cached
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   i <- x$getSolve()
