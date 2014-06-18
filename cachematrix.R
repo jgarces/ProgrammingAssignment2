5b787aa25c618e1ba6c6d95f4d8865c48b9cb166
jgarces <garces_jose_luis@hotmail.com>
2014-06-19 00:15:33 +0800

    Added error handling for Singular and non Square

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,24 @@
 ## aMatrix <- stats::rnorm(16)
 ## dim(aMatrix) <- c(4,4)
 ## aCacheMatrix <- makeCacheMatrix(aMatrix)
-## cacheSolve(aCacheMatrix)
+## cacheSolve(aCacheMatrix) ## Calling cacheSolve(makeCacheMatrix(aMatrix)) produces a error
 ## cacheSolve(aCacheMatrix) ## Call a second time to confirm cache is returned
-## aMatrix %*% cacheSolve(aCacheMatrix) ## Confirm matrix returned is inverse 
+## aMatrix %*% cacheSolve(aCacheMatrix) ## Confirm matrix returned is inverse
+
+## Test a noninvertable Matrix error Handling
+## aSingularMatrix <- c(2,1,6,3)
+## dim(aSingularMatrix) <- c(2,2)
+## aCacheMatrix <- makeCacheMatrix(aSingularMatrix)
+## cacheSolve(aCacheMatrix)
+
+## Test a non square matrix error handling
+## aNonSquareMatrix <- c(1,2,3,4,5,6)
+## dim(aNonSquareMatrix) <- c(3,2)
+## aCacheMatrix <- makeCacheMatrix(aNonSquareMatrix)
+## cacheSolve(aCacheMatrix)
 
 ## This function create custom matrix object that can cache the inversion of the
-## matrix. Note this function expects a square numeric matrix 
-## TODO: Check the matrix is a square matrix
+## matrix. 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y){
@@ -27,16 +38,37 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function determines the inverse of a CacheMatrix or return previously determined
-## inverse that has been cached
+## inverse that has been cached. Note this function expects a square numeric matrix.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  i <- x$getSolve()
-  if(!is.null(i)){
-    message("getting cached data")
-    return(i)
+  matrixDimensions <- dim(x$get())
+  if (length(matrixDimensions) == 2 && matrixDimensions[1] == matrixDimensions[2] ){
+    i <- x$getSolve()
+    if(!is.null(i)){
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- tryCatch({
+      solve(data, ...)  
+    },warning = function(war){
+      #Catch warning
+    },error = function(err){
+      #Catch error
+      # If error message from solve contains singular then supplied matrix is noninvertable
+      if (grepl("singular", err)){
+        "Supplied matrix is noninvertible"
+      }
+      else{
+        err
+      }        
+    }, finally = {
+      #Finally
+    })
+    x$setSolve(i)
+    i    
   }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setSolve(i)
-  i
+  else{
+    print("Supplied matrix is not a square matrix")
+  }
 }
